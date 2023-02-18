46e8eaa66844423a7d756e4f038910bcdea442c1
Janek <eunho.moon@gmail.com>
2023-02-18 16:58:06 +0900

    게시글 작성 및 대용량 데이터 insert 테스트

---
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostReadService.java
@@ -0,0 +1,21 @@
+package com.example.fastcampusmysql.domain.post.service;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountResponse;
+import com.example.fastcampusmysql.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostReadService {
+
+    private final PostRepository postRepository;
+
+    public List<DailyPostCountResponse> getDailyPostCount(DailyPostCountRequest request) {
+        return postRepository.groupByCreatedDate(request);
+    }
+
+}
